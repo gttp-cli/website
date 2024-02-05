7167890e47f16cea3fc0ab843a8ea864cf451d3c
MarvinJWendt <git@marvinjwendt.com>
2024-02-05 19:01:38 +0100

    feat: added /demo.yml route

---
+++ web/src/routes/demo.yml/+server.ts
@@ -0,0 +1,22 @@
+import { error } from '@sveltejs/kit';
+
+export async function GET() {
+    const schemaURL = "https://raw.githubusercontent.com/gttp-cli/gttp/main/_examples/demo.yml";
+
+    try {
+        const response = await fetch(schemaURL);
+        if (!response.ok) {
+            throw new Error('Failed to fetch schema');
+        }
+        const data = await response.json();
+
+        return new Response(JSON.stringify(data), {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    } catch (e) {
+        // Handle errors or invalid responses
+        throw error(500, `Error fetching schema: ${e.message}`);
+    }
+}
